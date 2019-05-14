e4d65204550587e30601bda1ed43b01bc2aa4d2d
c402132 <nataliia.derevianko@ibm.com>
2019-05-14 11:14:10 +0300

    First Commit

---
+++ src/test/java/Task_2_types_converting/Division_with_the_balance_1.java
@@ -0,0 +1,18 @@
+package test.java.Task_2_types_converting;
+
+public class Division_with_the_balance_1 {
+    public static void main(String[] args) {
+        int q = 21;
+        int w = 8;
+        int d = q / w;
+        int e = q % w;
+        System.out.println(q +  " / " + w +" = " + d + " and  " + e + "reminder of division");
+    }
+}
+
+
+
+
+
+
+
